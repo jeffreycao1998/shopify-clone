d86bf2f8a7c9154e262af3c2c498ef9dabf0fb13
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-12-22 23:00:10 -0500

    integrated graphql to server side and client side

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// GraphQL
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
+
+apolloClient
+  .query({
+    query: gql`
+      query books {
+        books {
+          title
+          author
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +31,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
