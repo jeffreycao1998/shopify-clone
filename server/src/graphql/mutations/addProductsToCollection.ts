c01f34a21b340f64ba3f91595bf2c7b5b7a66d7e
jeffreycao1998 <jeffreycao1998@hotmail.com>
2020-12-30 19:38:41 -0500

    doens't add duplicate products to collections

--- server/src/graphql/mutations/addProductsToCollection.ts
+++ server/src/graphql/mutations/addProductsToCollection.ts
@@ -1,4 +1,5 @@
-import { getCollectionsByUserId, createCollection, addProductToCollection } from '../../db/helpers';
+import { any } from 'sequelize/types/lib/operators';
+import { getProductsByCollectionId, addProductToCollection } from '../../db/helpers';
 import { ImageType, CollectionType, ContextType } from '../../types';
 
 type Args = {
@@ -8,12 +9,21 @@ type Args = {
 
 const addProductsToCollection = async (obj: {}, args: Args, context: ContextType) => {
   const { productIds, collectionId } = args;
-  // console.log(collectionId);
-  productIds.forEach(async (productId) => {
-    await addProductToCollection(productId, collectionId);
-  });
 
-  return { success: true };
+  const result = await getProductsByCollectionId(collectionId);
+  const productsInCollection = result.map((productCollection: any) => productCollection.dataValues.productId);
+  const newProductsToAdd = productIds.filter(productId => !productsInCollection.includes(productId));
+  console.log(productsInCollection, newProductsToAdd);
+
+  if (newProductsToAdd.length === 0) {
+    throw new Error('This collection already contains all selected products');
+  } else {
+    productIds.forEach(async (productId) => {
+      await addProductToCollection(productId, collectionId);
+    });
+  };
+
+  return { productsAdded: newProductsToAdd };
 };
 
 export default addProductsToCollection;
