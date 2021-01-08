ac20857f9df2edfd7d273ddbb6fa33da3e3f4533
jeffreycao1998 <jeffreycao1998@hotmail.com>
2021-01-08 18:50:32 -0500

    add example.env and change the email in a migration

--- server/seeders/20201229001027-demo-user.js
+++ server/seeders/20201229001027-demo-user.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [{
-      email: 'jeffreycao1998@hotmail.com',
+      email: 'example@hotmail.com',
       password: '$2b$10$pdSzomzx9ieXHMdaryEoVONLJnjBU32RAEh76zcQ0NP/zZ69TT6Pq', // password is 123
       createdAt: new Date(),
       updatedAt: new Date()
